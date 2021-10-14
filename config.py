53a3c1dc47ea3f0609da7c872a7a3b392291f71e
drtnf <tim.french@uwa.edu.au>
2021-10-14 11:50:22 +0800

    game server

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir,'.env'))
+
+class Config(object):
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'sshh!'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+  ENV='production'
+
+class DevelopmentConfig(Config):
+  FLASK_ENV='development'
+  DEBUG=True
+
+class TestingConfig(Config):
+  ENV='testing'
+  TESTING=True
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' #in memory database
